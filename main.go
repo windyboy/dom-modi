c5a469f48087f8a75c77dd83d22645526284ea73
zhiqiang feng <windy@windy-mbp.local>
2019-07-30 14:31:31 +0800

    add main process

--- main.go
+++ main.go
@@ -4,60 +4,141 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 
-	"github.com/urfave/cli/altsrc"
-	"gopkg.in/urfave/cli.v1"
+	"github.com/integrii/flaggy"
+	"gopkg.in/yaml.v2"
 )
 
-func main() {
-	app := cli.NewApp()
+// Declare variables and their defaults
+var config = "cfg.yml"
+var sourceFileName = "domain.xml"
+var sourceFile string
+var targetFile string
+var verbose bool
+var targetDir = "modify/"
+var overWrite bool
+var rules []Expression
 
-	flags := []cli.Flag{
-		/*
-		   // OK
-		   altsrc.NewIntFlag(cli.IntFlag{Name: "test", Value: -1}),
-		   altsrc.NewStringFlag(cli.StringFlag{Name: "flag", Value: "default"}),
-		*/
+//Expression config
+type Expression struct {
+	Name       string
+	Enable     bool
+	Expression string
+	Replace    string
+}
 
-		// NG #1
-		altsrc.NewIntFlag(cli.IntFlag{Name: "test, t", Value: -1}),
-		altsrc.NewStringFlag(cli.StringFlag{Name: "flag", Value: "default"}),
-		altsrc.NewBoolFlag(cli.BoolFlag{Name: "change"}),
+// LoadConfig read config file content
+func LoadConfig(fileLoc string) (string, error) {
+	content, err := ioutil.ReadFile(config)
+	return string(content), err
+}
 
-		/*
-		   // NG #2
-		   altsrc.NewIntFlag(cli.IntFlag{Name: "test", Value: -1}),
-		   altsrc.NewStringFlag(cli.StringFlag{Name: "flag, f", Value: "default"}),
+// MarshalConfig config yaml file
+func MarshalConfig(config string) ([]Expression, error) {
+	r := []Expression{}
+	err := yaml.Unmarshal([]byte(config), &r)
+	return r, err
+}
 
-		   // NG #3
-		   altsrc.NewIntFlag(cli.IntFlag{Name: "test, t", Value: -1}),
-		   altsrc.NewStringFlag(cli.StringFlag{Name: "flag, f", Value: "default"}),
-		*/
-
-		cli.StringFlag{Name: "conf, c", Value: "cfg.yml"},
-	}
-
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("yaml ist rad")
-		fmt.Printf("config: %s\n", c.String("conf"))
-		fmt.Printf("test: %d\n", c.Int("test"))
-		fmt.Printf("flag: %s\n", c.String("flag"))
-		fmt.Printf("change: %t\n", c.Bool("change"))
-
-		args := c.Args()
-		fmt.Println(args)
-		var domXml, err := ioutil.ReadFile(args.Get(0))
-		if err != nil {
-			fmt.Printf("read file error ", err)
-			return err
+// FindExpressionByName get reglular expression from config by name
+func FindExpressionByName(rule []Expression, name string) Expression {
+	for _, exp := range rule {
+		if name == exp.Name {
+			return exp
 		}
-		fmt.Println(string(domXml))
-		return nil
+	}
+	return Expression{}
+}
+
+func matchAndReplace(source string, rule Expression) string {
+	if rule.Enable {
+		expression, err := regexp.Compile(rule.Expression)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "expression compile error %v - expression %s\n", err, expression)
+			return source
+		} else if expression.MatchString(sourceFile) {
+			fmt.Printf("find match with rule [%s]", rule.Name)
+			return expression.ReplaceAllString(source, rule.Replace)
+		}
+	}
+	return source
+}
+
+func main() {
+
+	// set a description, name, and version for our parser
+	p := flaggy.NewParser("dom-modi")
+	p.Description = "modify the kvm domain xml exported from centos kvm."
+	p.Version = "0.0.1"
+	// display some before and after text for all help outputs
+	// p.AdditionalHelpPrepend = "I hope you like this program!"
+	// p.AdditionalHelpAppend = "This command has no warranty."
+
+	// add a positional value at position 1
+	p.AddPositionalValue(&sourceFileName, "source", 1, true, "kvm domain xml file location")
+
+	// create a subcommand at position 2
+	// you don't have to finish the subcommand before adding it to the parser
+	modifyCmd := flaggy.NewSubcommand("modify")
+	modifyCmd.Description = "modify the source file"
+
+	modifyCmd.String(&config, "c", "config", "rules of modification")
+	modifyCmd.String(&targetDir, "t", "target", "modified file save location")
+
+	p.AttachSubcommand(modifyCmd, 2)
+
+	// add a bool flag to the root command
+	p.Bool(&verbose, "v", "verbose", "detail information")
+
+	p.Bool(&overWrite, "o", "overwrite", "overwrite the target file")
+
+	p.Parse()
+
+	//load source file
+	sourceFileBytes, err := ioutil.ReadFile(sourceFileName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "source file %s error %v\n", sourceFileName, err)
+		os.Exit(1)
+	}
+	sourceFile = string(sourceFileBytes)
+	if verbose {
+		fmt.Printf("source %s loaded %d", sourceFileName, len(sourceFile))
 	}
 
-	app.Before = altsrc.InitInputSourceWithContext(flags, altsrc.NewYamlSourceFromFlagFunc("conf"))
-	app.Flags = flags
+	if _, err := os.Stat(targetDir); os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "target directory %s is not exists ", targetDir)
+		os.Exit(1)
+	}
 
-	app.Run(os.Args)
+	// content, err := ioutil.ReadFile(config)
+	content, err := LoadConfig(config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// r, err := mashalConfig(string(content))
+	rules, err := MarshalConfig(content)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// fmt.Println(len(rules))
+	targetFile = sourceFile
+	for _, rule := range rules {
+		fmt.Printf("[%s] > enable[%t] \n", rule.Name, rule.Enable)
+		targetFile = matchAndReplace(targetFile, rule)
+	}
+	fullTargetFile := targetDir + "/" + sourceFileName
+	if err := ioutil.WriteFile(fullTargetFile, []byte(targetFile), 0644); err != nil {
+		fmt.Fprintf(os.Stderr, "wirte modified content failed %v\n", err)
+		os.Exit(1)
+	} else {
+		fmt.Printf(" file [%s] wirte %d bytes", fullTargetFile, len(targetFile))
+	}
+	// fmt.Println(sourceFile, config, targetDir, verbose)
 
 }
